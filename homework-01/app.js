a2c28e16292d27ddec4cf1046030dc595ace225b
orbeet <excenter8@gmail.com>
2020-01-23 19:17:25 +0200

    new

---
+++ homework-01/app.js
@@ -0,0 +1,9 @@
+const message = 'JavaScript is awesome!';
+
+console.log(message); // JavaScript is awesome!
+
+const name = 'Mango';
+
+console.log('My name is: ', name); // My name is: Mango
+
+alert(message);
