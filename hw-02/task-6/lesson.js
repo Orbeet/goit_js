20c7a4023f06d9c8b4dfb188ed742e32296d66f6
orbeet <excenter8@gmail.com>
2020-02-11 07:26:09 +0200

    hw-3

---
+++ hw-02/task-6/lesson.js
@@ -0,0 +1,5 @@
+const text = 'Hello my name is Alex!';
+
+// h-1
+// e-3
+// l-3
--- hw-02/task-6/task-6.js
+++ hw-02/task-6/task-6.js
@@ -1,20 +1,39 @@
+// 'use strict';
+
+// let input;
+// const numbers = [];
+// let total = 0;
+
+// do {
+//   input = prompt('Введите число');
+
+//   numbers.push(Number(input));
+//   console.log(numbers);
+// } while (input !== null);
+
+// if (numbers.length > 0) {
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     total += numbers[i];
+//   }
+// }
+
+// alert(`Общая сумма чисел равна ${total}`);
+
 'use strict';
 
-let input;
-const numbers = [];
+const YOU_NUMBER = 'You number';
+const NO_NUMBER = 'Было введено не число, попробуйте еще раз';
+let userInput;
 let total = 0;
+const numbers = [];
 
-do {
-  input = prompt('Введите число');
-
-  numbers.push(Number(input));
-  console.log(numbers);
-} while (input !== null);
-
-if (numbers.length > 0) {
-  for (let i = 0; i < numbers.length; i += 1) {
-    total += numbers[i];
+while (userInput !== null) {
+  userInput = prompt(YOU_NUMBER, 0);
+  if (userInput === null) break;
+  const userNumber = +userInput;
+  if (Number.isNaN(userNumber)) {
+    alert(NO_NUMBER);
+  } else {
+    numbers.push(userNumber);
   }
 }
-
-alert(`Общая сумма чисел равна ${total}`);
