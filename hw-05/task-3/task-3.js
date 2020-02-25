918276a00bc3babb13c22033a7735fcf2b3fdfff
orbeet <excenter8@gmail.com>
2020-02-25 12:45:31 +0200

    hw-5

---
+++ hw-05/task-3/task-3.js
@@ -0,0 +1,41 @@
+'use strict';
+
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {
+    const arrLength = this.items.length;
+    for (let i = 0; i < arrLength; i += 1) {
+      const deleteItem = this.items[i];
+      if (deleteItem === item) {
+        this.items.splice(i, 1);
+      }
+    }
+  }
+}
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
